74dd88178bcf1aada205960222a01d82b13dc4f8
Murangira <theoneste@yahoo.com>
2025-11-15 08:32:43 +0200

    repo created

---
+++ puzzle.py
@@ -0,0 +1,72 @@
+from logic import *
+# Definition of symbols
+H=Symbol("Heather Attended the meeting")
+I=Symbol("Heather was invited")
+B=Symbol("The Boss wanted Heather at the meeting")
+F=Symbol("Heather is going to be fired")
+# Example of Knowledge base
+knowledge_h=And(Or(H,Not(I)),Implication(B,I),Not(H),
+                Implication(And(Not(B),Not(I)),F),)
+print(knowledge_h.symbols()) #Prints the set of symbols
+print(knowledge_h.formula())# Prints a sentence as logic formula
+print(model_check(knowledge_h,F))# Returns True if Knowledge base entails query
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    # TODO
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    # TODO
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    # TODO
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    # TODO
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
